d19bdbb5d740d4e8fca331165a5842ffbf43a7ce
Easibaja <easibaja06@gmail.com>
2025-03-14 14:05:53 -0600

    Proyect created and lab 1 solution

---
+++ Laboratoria1_B36655/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratoria1_B36655.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
